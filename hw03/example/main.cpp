107724ed6c57f461985a4960e0a03eeb631cd424
Jonnyhuan <fudanhyx@163.com>
2017-11-25 16:46:14 +0100

    Exercise 3, draw triangles using vertex and fragment rendering

--- hw03/example/main.cpp
+++ hw03/example/main.cpp
@@ -7,16 +7,45 @@ using namespace glm;
 using namespace agp;
 
 GLuint g_default_vao = 0;
+GLuint shaderProgram = 0;
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f, // left  
+    0.5f, -0.5f, 0.0f, // right 
+    0.0f,  0.5f, 0.0f  // top   
+}; 
+
+unsigned int VBO, VAO;
+
 
 void init()
 {
-    // Generate and bind the default VAO
-    glGenVertexArrays(1, &g_default_vao);
-    glBindVertexArray(g_default_vao);
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+  	// ------------------------------------------------------------------
+	shaderProgram = util::loadShaders("./vertexShader.glsl", "./fragmentShader.glsl");
+
+  	glGenVertexArrays(1, &VAO);
+  	glGenBuffers(1, &VBO);
+  	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+  	glBindVertexArray(VAO);
+	
+  	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+  	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  	glEnableVertexAttribArray(0);
+	
+  	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+  	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+	
+  	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+  	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+ 	glBindVertexArray(0); 
    
     // Set the background color (RGBA), change to color black
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
     // Your OpenGL settings, such as alpha, depth and others, should be
     // defined here! For the assignment, we only ask you to enable the
     // alpha channel.
@@ -24,8 +53,11 @@ void init()
 
 void release()
 {
-    // Release the default VAO
-    glDeleteVertexArrays(1, &g_default_vao);
+
+    // de-allocate all resources once they've outlived their purpose:
+  	// ------------------------------------------------------------------------
+  	glDeleteVertexArrays(1, &VAO);
+  	glDeleteBuffers(1, &VBO);
     
     // Do not forget to release any memory allocation here!
 }
@@ -34,8 +66,13 @@ void display()
 {
     // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
- //   printf("FreeGLUT triggered the display() callback!\n");
+
+    // draw our first triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+ 	//   printf("FreeGLUT triggered the display() callback!\n");
     
     // Your rendering code must be here! Do not forget to swap the
     // front and back buffers, and to force a redisplay to keep the
@@ -77,7 +114,6 @@ int main(int argc, char **argv)
     // Setup the window (e.g., size, display mode and so on)
     glutInitWindowSize(1280, 720);
     // glutInitWindowPosition( ... );
- //   glutInitDisplayMode(GLUT_RGBA);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //Initialized display mode to enable double buffering and RGBA
     
     // Make FreeGLUT return from the main rendering loop
@@ -102,6 +138,7 @@ int main(int argc, char **argv)
     
     // Display OpenGL information
     util::displayOpenGLInfo();
+
     
     // Initialize the 3D view
     init();
